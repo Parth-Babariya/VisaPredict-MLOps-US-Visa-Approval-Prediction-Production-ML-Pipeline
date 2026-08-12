4ef5876ab68c53a03a476b939dd8a03086101e70
Parth Babariya <parthbabariya0000@gmail.com>
2026-08-12 07:15:09 +0530

    Added structure, requirement and MangoDB

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Parth",
+    author_email="parthbabariya0000@gmail.com",
+    packages=find_packages()
+)
